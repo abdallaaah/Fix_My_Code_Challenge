9966b409b6ab3fa15cdcb2566f107ef47a3278a9
abdalla <eng.abdallaeltawil@gmail.com>
2023-10-28 06:52:31 -0700

    chec

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -52,9 +52,10 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        elif self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        else:
+            return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
 if __name__ == '__main__':
@@ -69,22 +70,22 @@ if __name__ == '__main__':
         print("User.id should be unique")
 
     u_pwd = "myPassword"
-    user_1.password = u_pwd
+    user_1.__password = u_pwd
     if user_1.password == u_pwd:
         print("User.password should be hashed")
 
     if user_2.password is not None:
         print("User.password should be None by default")
 
-    user_2.password = None
+    user_2.__password = None
     if user_2.password is not None:
         print("User.password should be None if setter to None")
 
-    user_2.password = 89
+    user_2.__password = 89
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if not user_1.is_valid_password(u_pwd):
+    if user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
