0c1d58ad71f805f20ff61e3836b9975e037861f0
abdalla <eng.abdallaeltawil@gmail.com>
2023-10-28 06:30:06 -0700

    check rubt

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -17,11 +17,12 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-          if result[i] < i_arg
-            i += i
+        if result[i] < i_arg
+            i += 1
         else
             result.insert(i, i_arg)
             is_inserted = true
+            break
         end
     end
     result << i_arg if !is_inserted
